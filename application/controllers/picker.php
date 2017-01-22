c109d19b7cc0ae256146301022f051bc5df78951
Marlon <boehland@hs-furtwangen.de>
2017-01-22 13:49:43 +0100

    Version 0.1 + Weiche zwischen PickApp und Auftragsbearbeitung

---
+++ application/controllers/picker.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * User: Marlon
+ * Date: 11.12.2016
+ * Time: 22:16
+ * Picker Controller
+ */
+class Picker extends Controller
+{
+    /**
+     * zuschnitt constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->view->render('mercury/index');
+    }
+}
---
+++ application/views/mercury/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Mercury Main Menu
+ * User: Marlon
+ * Date: 22.01.2017
+ * Time: 13:48
+ */
+echo "Hallo " . $_SESSION['Username'] . ", willkommen bei Mercury.";

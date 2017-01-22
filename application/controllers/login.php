06b0b08f03d3c8e1216d2714c23e52ba20503d66
Marlon <boehland@hs-furtwangen.de>
2017-01-22 22:46:36 +0100

    Version 0.2.3 + Datenbankanbindung

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -13,10 +13,6 @@ class Login extends Controller
     public function __construct()
     {
         parent::__construct();
-    }
-
-    public function index()
-    {
         $this->view->render('login/index');
     }
 }

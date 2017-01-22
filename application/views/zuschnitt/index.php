8b2e07c87b129a3b700d7f9863c309445adcd3d9
Marlon <boehland@hs-furtwangen.de>
2017-01-22 22:24:38 +0100

    Version 0.2.2 + Auftragsbearbeitung

--- application/views/zuschnitt/index.php
+++ application/views/zuschnitt/index.php
@@ -1,19 +1,25 @@
 <!-- Top Bar -->
 <div class="row">
-    <div class="col-sm-2"><img src="" class="img img-responsive img-square"></div>
+    <div class="col-sm-2"><img
+            src="https://www.stoff4you.de/out/pictures/master/product/1/fn1094-love-universalstoff-_z1.jpg"
+            class="img img-responsive img-square"></div>
     <div class="col-sm-7">
         <div class="row">
             <div class="col-sm-3">Artikel (Variante)</div>
             <div class="col-sm-4">Universalstoff (rot)</div>
+            <div class="clearfix"></div>
 
             <div class="col-sm-3">Art.Nr.</div>
             <div class="col-sm-4">60003001</div>
+            <div class="clearfix"></div>
 
             <div class="col-sm-3">EAN</div>
             <div class="col-sm-4">01234567890123</div>
+            <div class="clearfix"></div>
 
             <div class="col-sm-3">Lagerplatz</div>
             <div class="col-sm-4">H1-C-01</div>
+            <div class="clearfix"></div>
 
             <div class="col-sm-3">Lagerbestand</div>
             <div class="col-sm-4">145</div>

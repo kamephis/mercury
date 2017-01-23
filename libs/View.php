feec92e474bc7f64709b0b0376f443aa52490cfe
Marlon <boehland@hs-furtwangen.de>
2017-01-23 20:30:52 +0100

    Version 0.2.4 + MVC aktualisiert

--- libs/View.php
+++ libs/View.php
@@ -5,12 +5,10 @@ class View
 
     function __construct()
     {
-        echo 'this is the view';
     }
 
     public function render($name, $noInclude = false)
     {
-        echo $name;
         require 'views/' . $name . '.php';
     }
 }

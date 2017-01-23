e9f5c9c741757ced4b14e210f4cb10170de80cb7
Marlon <boehland@hs-furtwangen.de>
2017-01-23 01:27:58 +0100

    Version 0.2.4 + MVC aktualisiert

--- application/views/zuschnitt/index.php
+++ application/views/zuschnitt/index.php
@@ -6,24 +6,26 @@
             class="img img-responsive img-square"></div>
     <div class="col-sm-7">
         <div class="row">
-            <div class="col-sm-3">Artikel (Variante)</div>
-            <div class="col-sm-4">Universalstoff (rot)</div>
-            <div class="clearfix"></div>
+            <div class="well">
+                <div class="col-sm-3">Artikel (Variante)</div>
+                <div class="col-sm-4">Universalstoff (rot)</div>
+                <div class="clearfix"></div>
 
-            <div class="col-sm-3">Art.Nr.</div>
-            <div class="col-sm-4">60003001</div>
-            <div class="clearfix"></div>
+                <div class="col-sm-3">Art.Nr.</div>
+                <div class="col-sm-4">60003001</div>
+                <div class="clearfix"></div>
 
-            <div class="col-sm-3">EAN</div>
-            <div class="col-sm-4">01234567890123</div>
-            <div class="clearfix"></div>
+                <div class="col-sm-3">EAN</div>
+                <div class="col-sm-4">01234567890123</div>
+                <div class="clearfix"></div>
 
-            <div class="col-sm-3">Lagerplatz</div>
-            <div class="col-sm-4">H1-C-01</div>
-            <div class="clearfix"></div>
+                <div class="col-sm-3">Lagerplatz</div>
+                <div class="col-sm-4">H1-C-01</div>
+                <div class="clearfix"></div>
 
-            <div class="col-sm-3">Lagerbestand</div>
-            <div class="col-sm-4">145</div>
+                <div class="col-sm-3">Lagerbestand</div>
+                <div class="col-sm-4">145</div>
+            </div>
         </div>
     </div>
     <div class="col-sm-3">

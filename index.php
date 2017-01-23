c1077d7e0501b12623f9445b02daf14a7a6e6f72
Marlon <boehland@hs-furtwangen.de>
2017-01-23 17:40:40 +0100

    Version 0.2.4 + MVC aktualisiert

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php
-//session_start();
-error_reporting(E_ALL);
-require_once('application/libs/bootstrap.php');
-require_once('application/libs/model.php');
-require_once('application/libs/controller.php');
-require_once('application/libs/view.php');
-require_once('application/libs/lang.php');
-require_once('application/libs/dBase.php');
-$bs = new Bootstrap();
-$controller = new Controller();
+
+require 'config.php';
+require 'util/Auth.php';
+
+// Also spl_autoload_register (Take a look at it if you like)
+function __autoload($class)
+{
+    require LIBS . $class . ".php";
+}
+
+
+// Load the Bootstrap!
+$bootstrap = new Bootstrap();
+
+// Optional Path Settings
+//$bootstrap->setControllerPath();
+//$bootstrap->setModelPath();
+//$bootstrap->setDefaultFile();
+//$bootstrap->setErrorFile();
+
+$bootstrap->init();
---
+++ models/help_model.php
@@ -0,0 +1,16 @@
+<?php
+
+class Help_Model extends Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function blah()
+    {
+        return 10 + 10;
+    }
+
+}

4a965344bfff5cf56baee00a4162921c3176d587
Marlon <boehland@hs-furtwangen.de>
2017-01-23 20:17:55 +0100

    Version 0.2.4 + MVC aktualisiert

--- controllers/login.php
+++ controllers/login.php
@@ -11,9 +11,8 @@ class Login extends Controller
     function index()
     {
         $this->view->title = 'Login';
-
         $this->view->render('header');
-        $this->view->render('login/index');
+        //$this->view->render('login/index');
         $this->view->render('footer');
     }
 
@@ -21,6 +20,4 @@ class Login extends Controller
     {
         $this->model->run();
     }
-
-
 }

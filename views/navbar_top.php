6ac406a53f87ce280772d47972b4d9eee525038f
Marlon <boehland@hs-furtwangen.de>
2017-01-23 23:50:26 +0100

    Version 0.2.4 + MVC aktualisiert

---
+++ views/navbar_top.php
@@ -0,0 +1,36 @@
+<div class="row">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Navigation ein/ausblenden</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-leaf small"></span>&nbsp;Mercury
+                <small style="color:red;">beta</small>
+            </a>
+        </div>
+        <!-- ./ Header -->
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li>
+
+                </li>
+
+                <li>
+
+                </li>
+
+                <li>
+
+                </li>
+            </ul>
+        </div>
+        <!--/.nav-collapse -->
+    </nav>
+</div>
+<br>
+<br>
+<br>

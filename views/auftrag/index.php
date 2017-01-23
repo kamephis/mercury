b98b5b7c3dbe1920c57769ee9716732a112a40c9
Marlon <boehland@hs-furtwangen.de>
2017-01-23 23:37:07 +0100

    Version 0.2.4 + MVC aktualisiert

--- views/auftrag/index.php
+++ views/auftrag/index.php
@@ -69,13 +69,13 @@
     <div class="col-sm-2">12m</div>
 
     <div class="col-sm-2">
-        <button type="submit" class="btn btn-danger btn-block btn-lg">
+        <button type="submit" class="btn btn-danger btn-block btn-lg pull-right">
                 <span class="glyphicon glyphicon-remove"></span>
             </button>
     </div>
 
     <div class="col-sm-2">
-        <button type="submit" class="btn btn-success btn-block btn-lg">
+        <button type="submit" class="btn btn-success btn-block btn-lg pull-right">
                 <span class="glyphicon glyphicon-ok"></span>
             </button>
     </div>

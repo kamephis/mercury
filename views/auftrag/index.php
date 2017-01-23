adb862835666182ab2cc646b7d7c698dcd0d1a70
Marlon <boehland@hs-furtwangen.de>
2017-01-24 00:25:16 +0100

    Version 0.2.4 + MVC aktualisiert

--- views/auftrag/index.php
+++ views/auftrag/index.php
@@ -163,3 +163,70 @@
     </div>
 </div>
 <!-- ./ Auftragspositionen -->
+
+
+<!-- modal test -->
+
+<div id="modFehler" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Fehlbestand melden</h4>
+            </div>
+            <div class="modal-body">
+                <p>Bitte wählen Sie die passende Option:</p>
+                <div class="row">
+                    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?> ?>" class="form-horizontal">
+                        <div class="well-sm">
+                            <div class="checkbox">
+                                <label><input type="checkbox" value="">Fehlmenge</label>
+                            </div>
+
+                            <div class="checkbox">
+                                <label><input type="checkbox" value="">Artikel defekt</label>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default btn-block btn-lg" data-dismiss="modal">Schließen
+                </button>
+                <button type="button" class="btn btn-success btn-block btn-lg" data-dismiss="modal">Bestätigen
+                </button>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- ./ modal test -_>
+
+
+   <!-- modal test -->
+
+<div id="modPicked" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Pick bestätigen</h4>
+            </div>
+            <div class="modal-body">
+                <h1 class="text-center"><b>3 m</b></h1>
+                <h2 class="text-center"><b>gepickt?</b></h2>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default btn-block btn-lg" data-dismiss="modal">NEIN</button>
+                <button type="button" class="btn btn-success btn-block btn-lg" data-dismiss="modal">JA</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- ./ modal test -_>
+

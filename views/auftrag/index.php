1c3fd106aa4e904d41a4f7c02c3222d6c47626bc
Marlon <boehland@hs-furtwangen.de>
2017-01-23 23:04:04 +0100

    Version 0.2.4 + MVC aktualisiert

--- views/auftrag/index.php
+++ views/auftrag/index.php
@@ -8,7 +8,7 @@
                     src="https://www.stoff4you.de/out/pictures/master/product/1/fn1094-love-universalstoff-_z1.jpg"
                     width="150px"
                     class="img img-responsive img-square"></div>
-            <div class="col-sm-7">
+            <div class="col-sm-4">
                 <div class="row">
                     <div class="col-sm-3">Artikel (Variante)</div>
                     <div class="col-sm-4">Universalstoff (rot)</div>
@@ -17,7 +17,9 @@
                     <div class="col-sm-3">Art.Nr.</div>
                     <div class="col-sm-4">60003001</div>
                     <div class="clearfix"></div>
+                </div>
 
+                <div class="col-sm-4">
                     <div class="col-sm-3">EAN</div>
                     <div class="col-sm-4">01234567890123</div>
                     <div class="clearfix"></div>
@@ -30,7 +32,7 @@
                     <div class="col-sm-4">145</div>
                 </div>
             </div>
-            <div class="col-sm-3">
+            <div class="col-sm-2">
                 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" role="form" method="post">
                     <button type="submit" class="btn btn-lg btn-default btn-block pull-right">
                         <span class="glyphicon glyphicon-user"></span> Abmelden

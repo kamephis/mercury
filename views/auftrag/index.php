3cd55f9cdfa4741df99a4fb64ffeb07534e41e26
Marlon <boehland@hs-furtwangen.de>
2017-01-23 23:32:50 +0100

    Version 0.2.4 + MVC aktualisiert

--- views/auftrag/index.php
+++ views/auftrag/index.php
@@ -57,7 +57,7 @@
     <div class="col-sm-4"><b>Aktion</b></div>
 </div>
 <!-- Auftragspositionen -->
-<div class="row col-table">
+<div class="row row-table">
     <div class="col-sm-2">01</div>
 
     <div class="col-sm-4">

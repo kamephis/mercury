584d1ccc82f925184e599262756276774ad176cc
Marlon <boehland@hs-furtwangen.de>
2017-01-23 22:35:03 +0100

    Version 0.2.4 + MVC aktualisiert

--- views/help/index.php
+++ views/help/index.php
@@ -1,5 +1,7 @@
-<h1>Help</h1>
-
-<p>
-    This is the help.
-</p>
+<div class="panel panel-primary">
+    <div class="panel-heading">Hilfe</div>
+    <div class="panel-body">
+        Mercury Hilfe
+    </div>
+    
+</div>

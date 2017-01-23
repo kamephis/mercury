09c663f33d28c1384cec39510045b5fd1b110b9f
Marlon <boehland@hs-furtwangen.de>
2017-01-23 20:55:18 +0100

    Version 0.2.4 + MVC aktualisiert

--- models/login_model.php
+++ models/login_model.php
@@ -9,26 +9,129 @@ class Login_Model extends Model
 
     public function run()
     {
-        $sth = $this->db->prepare("SELECT userid, role FROM user WHERE 
-                login = :login AND password = :password");
-        $sth->execute(array(
-            ':login' => $_POST['login'],
-            ':password' => Hash::create('sha256', $_POST['password'])
-        ));
+        $username = $_REQUEST['username'];
+        $password = $_REQUEST['password'];
+        $targetApp = $_REQUEST['targetApp'];
 
-        $data = $sth->fetch();
+        /**
+         * pruefen ob der Benutzer berechtigt ist
+         * Voraussetzung fuer den Zugriff auf das Backend ist mind. ein Eintrag in der Tabelle iRight2iUser
+         */
+        //$dbServer = 'mysql:dbname=' . $this->_dbName . ';host=' . $this->_dbHost . '; port=' . $this->_dbPort . '';
+        //$dbUser = $this->_dbUser;
+        //$dbPassword = $this->_dbPasswd;
+        // DB Connection
+        //$pdo = new PDO($dbServer, $dbUser, $dbPassword);
 
-        $count = $sth->rowCount();
+        $sql = $this->db->prepare("SELECT iUser.UID, iUser.Username, iUser.name, iUser.vorname, iUser.Passwd, iUser.RegDate, iUser.kuerzel FROM iUser WHERE Username = '{$username}'");
+        $sql->execute();
+        $totalRows = $sql->rowCount();
+        $result = $sql->fetch(PDO::FETCH_ASSOC);
 
-        if ($count > 0) {
-            // login
-            Session::init();
-            Session::set('role', $data['role']);
-            Session::set('loggedIn', true);
-            Session::set('userid', $data['userid']);
-            header('location: ../dashboard');
+        // Zuweisung der Rows
+        $userID = $result['UID'];
+        $vorname = $result['vorname'];
+        $name = $result['name'];
+
+        // Pruefen ob der Benutzername existiert
+        if ($totalRows > 0 || ($username == $result['Username'])) {
+            // Entschlüsselung des Kennworts. Wenn erfolgreich: Registrieren der Session Variablen
+            if ($result['Passwd'] == $this->decryptPassword($password, $result['RegDate'])) {
+
+                // Registrieren der Session Variablen fuer den User
+
+                @session_start();
+                $_SESSION['userName'] = $username;
+                $_SESSION['userID'] = $userID;
+                $_SESSION['vorname'] = $vorname;
+                $_SESSION['name'] = $name;
+
+                /**
+                 * Array mit den Benutzerrechten erzeugen und in die Session speichern
+                 */
+                $queryRights = $this->db->prepare("SELECT UID, RID FROM iRight2iUser WHERE UID = {$_SESSION['userID']}");
+                $queryRights->execute();
+                $arrayRights = array();
+
+                while ($rowRights = $queryRights->fetch(PDO::FETCH_ASSOC)) {
+                    $arrayRights[] = $rowRights['RID'];
+                }
+                $_SESSION['rights'] = $arrayRights;
+
+                $_SESSION['targetApp'] = $targetApp;
+                echo $_SESSION['targetApp'];
+
+                // Weiterleitung zur Startseite der Anwendung
+                if (isset($_SESSION['targetApp'])) {
+                    switch ($_SESSION['targetApp']) {
+                        case 'ab':
+                            header('location: /zuschnitt');
+                            break;
+
+                        case 'pick':
+                            header("Location: /picker");
+                            break;
+                    }
+                }
+            } else {
+                header("Location: http://dev.stoffpalette.com/pixiPickprozess/?e=401");
+            }
         } else {
-            header('location: ../login');
+            header("Location: http://dev.stoffpalette.com/pixiPickprozess/?e=401");
         }
+    }
+
+    /**
+     * @param $password
+     * @return string
+     */
+    public function encryptPassword($password)
+    {
+        $myDate = new MyDateTime();
+
+        // Aktueller Timestamp wird beim Aufruf der Funktion eingelesen und
+        // dann als Salt-Value in der Session Variablen regDate gespeichert.
+        // Auf diese wird dann in der registerNewUser zugegriffen.
+
+        $timeStamp = $myDate->getTimestamp();
+        $sid = session_id();
+
+        if (isset($sid)) {
+            $_SESSION['regDate'] = $timeStamp;
+        } else {
+            echo "Es ist keine Session aktiv!";
+        }
+
+        return hash('sha256', $password . $timeStamp);
+    }
+
+    /**
+     * @param $password
+     * @param $regDate
+     * @return string
+     */
+    public function decryptPassword($password, $regDate)
+    {
+        // Pruefung des eingegebenen Kennworts auf dessen Richtigkeit
+        $decrypt = hash('sha256', $password . $regDate);
+        return $decrypt;
+    }
+}
+
+/**
+ * Class MyDateTime
+ */
+class MyDateTime extends DateTime
+{
+    public function setTimestamp($timestamp)
+    {
+        $date = getdate(( int )$timestamp);
+        $this->setDate($date['year'], $date['mon'], $date['mday']);
+        $this->setTime($date['hours'], $date['minutes'], $date['seconds']);
+    }
+
+    public function getTimestamp()
+    {
+        return $this->format('U');
     }
 }

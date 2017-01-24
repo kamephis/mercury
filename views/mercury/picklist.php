b151b8cc8e0a43d56e4ff74ea1205793e1a99e36
Marlon <boehland@hs-furtwangen.de>
2017-01-24 22:55:35 +0100

    Version 0.2.4 + MVC aktualisiert

--- views/mercury/picklist.php
+++ views/mercury/picklist.php
@@ -1,9 +1,10 @@
 <div class="well-sm">
-    <?php
-    foreach ($this->PicklistItems as $item){
-    ?>
+
     <div class="row">
         <div class="col-xs-9">
+            <?php
+            foreach ($this->PicklistItems as $item){
+            ?>
             <div class="row">
                 <div class="col-xs-12 col-md-12 small">
                     <b>Lagerplatz</b>

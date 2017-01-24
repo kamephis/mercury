c5632440ed6540f0d932176b0ffad453a3c7ba3b
Marlon <boehland@hs-furtwangen.de>
2017-01-24 11:54:46 +0100

    Version 0.2.4 + MVC aktualisiert

--- views/auftrag/index.php
+++ views/auftrag/index.php
@@ -35,7 +35,7 @@
                         <span class="glyphicon glyphicon-user"></span> Abbrechen
                     </button>
                 </form>
-        <br>
+        <span style="height:1em; display:inline-block;">&nbsp;</span>
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" role="form" method="post">
                     <button type="submit" class="btn btn-success btn-block">
                         <span class="glyphicon glyphicon-check"></span> Abschließen

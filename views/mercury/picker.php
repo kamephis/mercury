bd9a90cac3fd91149b6788eead128a2c3747465e
Marlon <boehland@hs-furtwangen.de>
2017-01-24 22:04:07 +0100

    Version 0.2.4 + MVC aktualisiert

--- views/mercury/picker.php
+++ views/mercury/picker.php
@@ -1,23 +1,26 @@
-
-<div class="well-sm">
+<?php
+foreach ($this->masterPicklist as $pickList) {
+    ?>
+    <div class="well-sm">
     <!-- Anzeige Picklisten -->
     <div class="row">
         <div class="col-xs-12">
             <h1>Aktive Picklisten</h1>
-            <p class="lead">Hallo <?php echo Session::get('vorname'); ?>, folgende Picklisten sind für dich zum Picken
+            <p class="lead">Hallo <?php echo Session::get('vorname'); ?>, folgende Picklisten sind für dich zum
+                Picken
                 freigegeben.</p>
         </div>
-    </div>
+        </div>
 
     <div class="row">
         <div class="col-xs-3 col-md-1">
             <h4><b>#</b></h4>
             <div class="row">
                 <div class="col-xs-12">
-                    65584
+                    <?php echo $pickList['PLHkey']; ?>
+                </div>
                 </div>
             </div>
-        </div>
         <div class="col-xs-9 col-md-4 text-left">
             <h4><b>Picklisten Details</b></h4>
             <div class="row">
@@ -51,19 +54,20 @@
                 <div class="clearfix"></div>
                 <br>
                 <!-- smarter -->
+                </div>
             </div>
         </div>
-    </div>
     <div class="row">
         <div class="col-xs-12 col-md-12">
             <form action="<?php echo URL; ?>picklist" method="post" role="form">
                 <input type="hidden" name="p" value="picklist">
-                <button type="submit" class="btn btn-success btn-block btn-lg" name="btnStartPick" id="btnStartPick">
+                <button type="submit" class="btn btn-success btn-block btn-lg" name="btnStartPick"
+                        id="btnStartPick">
                     <span class="glyphicon glyphicon-time"></span> Picken beginnen
                 </button>
             </form>
         </div>
-    </div>
-</div><?php
-var_dump($this->masterPicklist);
+        </div>
+    </div><?php
+}
 ?>

c92f1c211f097a17e593a83176ea27d99388bb13
Marlon <boehland@hs-furtwangen.de>
2017-01-24 22:12:07 +0100

    Version 0.2.4 + MVC aktualisiert

--- views/mercury/picker.php
+++ views/mercury/picker.php
@@ -28,7 +28,7 @@ foreach ($this->masterPicklist as $pickList) {
                     Erstellt von:
                 </div>
                 <div class="col-xs-7">
-                    Böhland
+                    <?php echo $pickList['CreatedBy']; ?>
                 </div>
                 <div class="clearfix"></div>
                 <div class="col-xs-5 col-md-2 text-left">

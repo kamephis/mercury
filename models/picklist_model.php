b1d73af372809caed59d5a585e012518a55b9c3a
Marlon <boehland@hs-furtwangen.de>
2017-01-24 22:44:25 +0100

    Version 0.2.4 + MVC aktualisiert

--- models/picklist_model.php
+++ models/picklist_model.php
@@ -7,5 +7,14 @@ class Picklist_Model extends Model
         parent::__construct();
     }
 
-    
+    public function getPicklistItems($picklistID)
+    {
+        $sql = "SELECT pitem.*
+                FROM stpPicklistItems pitem
+                RIGHT JOIN stpArtikel2Pickliste a2p
+                ON (pitem.id = a2p.ArtikelID)
+                WHERE a2p.PicklistID = '{$picklistID}'";
+
+        return $this->db->select($sql);
+    }
 }

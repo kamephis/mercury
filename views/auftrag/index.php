a83dc3062550b72833b8896ab029ba48a1a20ac2
Marlon <boehland@hs-furtwangen.de>
2017-01-25 19:11:50 +0100

    Version 0.2.4 + MVC aktualisiert

--- views/auftrag/index.php
+++ views/auftrag/index.php
@@ -2,7 +2,7 @@
 // Übergabe via Barcode
 $auftrag = $this->auftrag->getAuftrag('10603480');
 //$pixiBestand = $this->auftrag->getPixiBestand($auftrag[0]['EanUpc']);
-$bestand = $this->auftrag->getPixiBestand($auftrag[0]['ItemNrInt']);
+$bestand = $this->Pixi->getPixiBestand($auftrag[0]['ItemNrInt']);
 
 //var_dump($this->Pixi->getAllPicklists());
 ?>

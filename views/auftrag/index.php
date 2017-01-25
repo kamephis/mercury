6407d24c6f3e76a8ae6568212869d747469aa9b9
Marlon <boehland@hs-furtwangen.de>
2017-01-25 19:05:11 +0100

    Version 0.2.4 + MVC aktualisiert

--- views/auftrag/index.php
+++ views/auftrag/index.php
@@ -4,8 +4,7 @@ $auftrag = $this->auftrag->getAuftrag('10603480');
 //$pixiBestand = $this->auftrag->getPixiBestand($auftrag[0]['EanUpc']);
 //$bestand = $this->auftrag->getPixiBestand($auftrag[0]['EanUpc']);
 
-//echo $this->Pixi->getItemStock('2147483647');
-echo getcwd();
+echo $this->Pixi->getItemStock('2147483647');
 ?>
 
 <!-- Top Bar -->

8c5d00442ee71c005d5eec06003d462a33b339ed
Marlon <boehland@hs-furtwangen.de>
2017-01-26 12:33:50 +0100

    Version 0.2.4 + MVC aktualisiert

--- views/login/password.php
+++ views/login/password.php
@@ -1,4 +1,27 @@
-<form method="post" action="login">
-    <input type="text" name="password">
-    <input type="submit" value="weiter">
-</form>
+<div class="panel col-xs-12 col-md-6 col-md-offset-3" id="loginbox">
+    <div class="panel-heading">
+        <img src="<?php echo IMG_PATH; ?>logo_600.jpg" class="img img-responsive">
+    </div>
+
+    <div class="panel-body loginPanel">
+        <form id="loginform" class="form-horizontal" role="form" method="post" action="login">
+            <input type="hidden" name="appTarget" value="<?php echo $appTarget; ?>">
+
+            <div class="input-group inputLogin">
+                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                <input id="login-password" type="password" class="form-control" name="passwd"
+                       placeholder=" Kennwort...<?php #echo $lang->aText['inputUserPasswordRequiredText'];?>" required>
+            </div>
+            <div style="margin-top:10px" class="form-group">
+                <div class="col-xs-12 col-sm-12">
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-3">
+                            <button type="submit" class="btn btn-success btn-block">
+                                Anmelden <?php #echo $lang->aText['buttonLoginText'];?></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>

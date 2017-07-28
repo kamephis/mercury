6e3a2b0a3642d2a76de374548fadbd1d02e97a28
Marlon <boehland@hs-furtwangen.de>
2017-07-28 11:28:42 +0200

    Version 1.0.4.2 ~ Fehlerliste übersichtlicher gestaltet

--- views/mobile/artikelfehlerMobile.php
+++ views/mobile/artikelfehlerMobile.php
@@ -8,37 +8,68 @@ if (isset($_REQUEST['itemPicked'])) {
 <div class="panel panel-primary">
     <div class="panel-heading">Artikel mit Fehlern</div>
     <div class="panel-body">
-        <div class="row">
-            <div class="col-xs-12">
                 <?php
                 foreach ($this->artikelFehler as $fehlerItem) {
                     ?>
                     <table class="table table-responsive table-striped table-condensed table-bordered">
                         <tr>
-                            <td>
-                                <h4><strong><?php echo utf8_encode($fehlerItem['ItemName']); ?></strong></h4>
-                                Art.Nr: <?php echo $fehlerItem['ItemNrSuppl']; ?><br>
-                                EAN: <?php echo $fehlerItem['EanUpc']; ?><br>
-                                Pickliste: <?php echo $fehlerItem['PLIheaderRef']; ?><br>
-                                Lagerplatz: <?php echo $fehlerItem['BinName']; ?><br>
-                                Best.Menge: <?php echo $fehlerItem['Qty']; ?><br>
-                                Fehler: <?php echo $fehlerItem['ItemFehler']; ?><br>
-                                Größte verf. Menge: <?php echo $fehlerItem['ItemFehlbestand']; ?>
-                            </td>
+                            <td colspan="2"><h4><strong><?php echo utf8_encode($fehlerItem['ItemName']); ?></strong>
+                                </h4></td>
+                        </tr>
+
                         <tr>
-                            <td>
-                                <form method="post" action="<?php echo $_SERVER['php_self']; ?>">
-                                    <input type="hidden" name="itemPicked" value="1">
-                                    <input type="hidden" name="EanUpc" value="<?php echo $fehlerItem['EanUpc']; ?>">
-                                    <input class="btn btn-lg btn-success btn-block" type="submit" id="btnPickItem"
-                                           name="btnPickItem" value="Artikel Picken">
-                                </form>
+                            <td colspan="2"><h4>
+                                    <strong><code>Lagerplatz: <?php echo $fehlerItem['BinName']; ?></code></strong></h4>
                             </td>
                         </tr>
+
+                        <tr>
+                            <td colspan="2">
+                                <img src="<?php echo $fehlerItem['ItemNrSuppl']; ?>"
+                                     class="img img-thumbnail img-responsive">
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>Art.Nr:</td>
+                            <td><?php echo $fehlerItem['ItemNrSuppl']; ?></td>
+                        </tr>
+
+                        <tr>
+                            <td>EAN</td>
+                            <td><?php echo $fehlerItem['EanUpc']; ?></td>
+                        </tr>
+
+                        <tr>
+                            <td>Pickliste</td>
+                            <td><?php echo $fehlerItem['PLIheaderRef']; ?><br></td>
+                        </tr>
+
+                        <tr>
+                            <td>Best.Menge</td>
+                            <td><?php echo $fehlerItem['Qty']; ?></td>
+                        </tr>
+
+                        <tr>
+                            <td>Fehler</td>
+                            <td><?php echo $fehlerItem['ItemFehler']; ?></td>
+                        </tr>
+
+                        <tr>
+                            <td>Größte verf. Menge</td>
+                            <td><?php echo $fehlerItem['ItemFehlbestand']; ?></td>
+                        </tr>
+
+                        <td colspan="2">
+                            <form method="post" action="<?php echo $_SERVER['php_self']; ?>">
+                                <input type="hidden" name="itemPicked" value="1">
+                                <input type="hidden" name="EanUpc" value="<?php echo $fehlerItem['EanUpc']; ?>">
+                                <input class="btn btn-lg btn-success btn-block" type="submit" id="btnPickItem"
+                                       name="btnPickItem" value="Artikel Picken">
+                            </form>
+                        </td>
                     </table>
                     <br>
                 <?php } ?>
-            </div>
-        </div>
     </div>
 </div>

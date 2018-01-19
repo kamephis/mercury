a529d040c1d1b14965460ee99cdfc616d35284ec
Marlon Böhland <mb@stoffpalette.com>
2018-01-19 15:41:24 +0100

    Version 1.0.3.7 + Picker Statistik Pie Chart

--- views/backend/statistik/stat_pick.php
+++ views/backend/statistik/stat_pick.php
@@ -1,7 +1,36 @@
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+    google.charts.load('current', {'packages': ['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+            <?php
+            $jsonPickedLists = $this->statistik->getPickStatistik();
+            ?>
+            ['Task', 'Hours per Day'],
+            <?php foreach ($jsonPickedLists as $jPick) { ?>
+            ['<?php echo utf8_encode($jPick['picker']); ?>', <?php echo $jPick['menge']; ?>],
+            <?php } ?>
+        ]);
+
+        var options = {
+            title: 'Verhältnis Picker / Picks Gesamt'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+    }
+</script>
+
 <div class="col-lg-6">
     <div class="panel panel-default">
         <div class="panel-heading">Picker-Auswertung</div>
         <div class="panel-body">
+            <div id="piechart" style="width: 800px; height: 350px;"></div>
+
             <form name="frmFilter">
                 <?php $aPicker = $this->back->getAllPicker(); ?>
                 <label>Mitarbeiter
@@ -26,31 +55,31 @@
             <div class="row">
                 <div class="col-lg-12">
                     <?php if (isset($_REQUEST['auftragsdatum']) || isset($_REQUEST['bearbeiter'])) {
-                    $aPickedLists = $this->statistik->getPickStatistik($_REQUEST['bearbeiter'], $_REQUEST['auftragsdatum']); ?>
-                    <table class="table table-responsive table-bordered table-striped">
-                        <thead>
-                        <tr>
-                            <th>Datum</th>
-                            <th>Bearbeiter</th>
-                            <th>Picks</th>
-                            <th>Dauer (Minuten)</th>
-                        </tr>
-                        </thead>
-
-                        <tbody>
-                        <?php foreach ($aPickedLists as $pickedList) { ?>
+                        $aPickedLists = $this->statistik->getPickStatistik($_REQUEST['bearbeiter'], $_REQUEST['auftragsdatum']); ?>
+                        <table class="table table-responsive table-bordered table-striped">
+                            <thead>
                             <tr>
-                                <td>
+                                <th>Datum</th>
+                                <th>Bearbeiter</th>
+                                <th>Picks</th>
+                                <th>Dauer (Minuten)</th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            <?php foreach ($aPickedLists as $pickedList) { ?>
+                                <tr>
+                                    <td>
                                         <span style="font-size:0.8em;"
                                               class="glyphicon glyphicon-calendar"></span>
-                                    <?php echo $pickedList['datum']; ?></td>
-                                <td><?php echo utf8_encode($pickedList['picker']); ?></td>
-                                <td><?php echo $pickedList['menge']; ?></td>
-                                <td><?php echo number_format($pickedList['dauer'], 2, ',', ' '); ?></td>
-                            </tr>
-                        <?php } ?>
-                        </tbody>
-                    </table>
+                                        <?php echo $pickedList['datum']; ?></td>
+                                    <td><?php echo utf8_encode($pickedList['picker']); ?></td>
+                                    <td><?php echo $pickedList['menge']; ?></td>
+                                    <td><?php echo number_format($pickedList['dauer'], 2, ',', ' '); ?></td>
+                                </tr>
+                            <?php } ?>
+                            </tbody>
+                        </table>
                     <?php } ?>
                 </div>
             </div>
